864d340d0c46b0a2dc9d256230ba426de58b70dc
Priyanshu Agarwal <109196864+priyanshu-101@users.noreply.github.com>
2024-09-03 00:17:53 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '8d2a110b6ad468ae1a0e459757cf659d';
+const API_KEY = '42b2460acbe04a20ad9184717240209';
 const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async (city, country) => {
